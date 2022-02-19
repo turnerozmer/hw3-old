a301ec1be12336ab4ec035d064b25a6599fdd335
turnerozmer <turner.ozmer@kellogg.northwestern.edu>
2022-02-19 23:33:42 +0000

    commit before starting over to try again

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :place
-  resources :post
+  resources :places
+  resources :posts
   # Defines the root path route ("/")
   # root "articles#index"
 end
